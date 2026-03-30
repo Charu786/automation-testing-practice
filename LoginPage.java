4899130d523d369a9424b8f97c29dfec253c9894
sanja <charusagar35@gmail.com>
2026-03-30 15:23:57 +0530

    LoginPage

--- LoginPage.java
+++ LoginPage.java
@@ -2,29 +2,45 @@ package automationtests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
 
     WebDriver driver;
+    WebDriverWait wait;
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
-    //Locators
-    By username = By.id("username");
-    By password = By.id("password");
-    By loginBtn = By.cssSelector("button[type='submit']");
-    By successMsg = By.id("flash");
+    //Locators using PageFactory
+    @FindBy(id = "username")
+    WebElement username;
+
+    @FindBy(id = "password")
+    WebElement password;
+
+    @FindBy(css = "button[type='submit']")
+    WebElement loginBtn;
+
+    @FindBy(id = "flash")
+    WebElement message;
     //Actions
-    public void enterUsername(String user) {
-        driver.findElement(username).sendKeys(user);
+    public void login(String user, String pass) {
+        username.sendKeys(user);
+        password.sendKeys(pass);
+        loginBtn.click();
     }
-    public void enterPassword(String pass) {
-        driver.findElement(password).sendKeys(pass);
-    }
-    public void clickLogin() {
-        driver.findElement(loginBtn).click();
-    }
-    public String getSuccessMessage() {
-        return driver.findElement(successMsg).getText();
+    public String getMessage() {
+        wait.until(ExpectedConditions.visibilityOf(message));
+        return message.getText();
     }
 }
